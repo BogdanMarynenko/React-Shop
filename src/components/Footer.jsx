be2d1905a76a3ccbff58bef9b25e20b146c5d0b6
BogdanMarynenko <109315129+BogdanMarynenko@users.noreply.github.com>
2022-12-25 13:33:35 +0200

    gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   <div className="footer-copyright">
     <div className="container">
     © {new Date().getFullYear()} Copyright Text
-    <a className="grey-text material-icons text-lighten-4 right My-git" href="https://github.com/BogdanMarynenko/React-Shop.git" target="_blank" rel="noreferrer">reply<i>MY GIT</i></a>
+    <a className="grey-text material-icons text-lighten-4 right My-git" href="https://github.com/BogdanMarynenko/React-Shop" target="_blank" rel="noreferrer">reply<i>MY GIT</i></a>
     </div>
   </div>
 </footer>
